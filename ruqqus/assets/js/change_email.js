7091b6f0d8f185232639001682e4216e7b4dce2e
kek7198 <memezdank140@gmail.com>
2019-09-24 10:46:43 -0500

    Update change_email.js

--- ruqqus/assets/js/change_email.js
+++ ruqqus/assets/js/change_email.js
@@ -2,9 +2,9 @@
 
 $('#new_email').on('input', function () {
 
-    var id = document.getElementById("password");
+    var id = document.getElementById("email-password");
     var id2 = document.getElementById("email-password-label");
-    var id3 = document.getElementById("passwordRequired");
+    var id3 = document.getElementById("emailpasswordRequired");
 
     id.classList.remove("d-none");
     id2.classList.remove("d-none");
