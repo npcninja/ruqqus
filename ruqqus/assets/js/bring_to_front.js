52fee2a78c9870009530cdbcc788bd32c8a6349b
kek7198 <memezdank140@gmail.com>
2019-10-07 13:50:39 -0500

    jquery

--- ruqqus/assets/js/bring_to_front.js
+++ ruqqus/assets/js/bring_to_front.js
@@ -1,3 +1,10 @@
-function bringToFront() {
-    this.style.zIndex = "4";
-}
+// Enlarge thumbnail on post-img click
+
+$(".post-actions a").click(function (event) {
+    event.preventDefault();
+
+    var id = $(this).parent().attr("id");
+
+    document.getElementById(id).style.zIndex = "4";
+
+});
