77076c98dd8028b5253b25b901d2c1d7da68c8b8
kek7198 <memezdank140@gmail.com>
2019-10-25 12:25:25 -0500

    Reduced state image file sizes

--- ruqqus/assets/js/collapse.js
+++
@@ -1,9 +0,0 @@
-// Toggle comment collapse
-
-$(".comment-collapse").click(function (event) {
-    event.preventDefault();
-
-    var id = $(this).parent().attr("id");
-
-    document.getElementById(id).classList.toggle("collapsed");
-});
