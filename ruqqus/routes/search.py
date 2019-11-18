5f363e724d641edbc66bdeebbf82289c4da58748
captainmeta4 <captainmeta4@gmail.com>
2019-11-18 13:33:24 -0500

    Update search.py

--- ruqqus/routes/search.py
+++ ruqqus/routes/search.py
@@ -9,16 +9,27 @@ from ruqqus.__main__ import app, db
 def search(v):
 
     term=request.args.get("q")
+    sort_method=request.args.get("sort", "hot").lower()
+    
     page=max(1, int(request.args.get("page", 1)))
 
     term="%"+term+"%"
 
-    search = db.query(Submission).filter_by(is_banned=False, is_deleted=False).filter(Submission.title.ilike(term)).offset(25*(page-1))
-    
-    total=search.count()
-    results=[p for p in search.limit(26)]
+    posts = db.query(Submission).filter_by(is_banned=False, is_deleted=False).filter(Submission.title.ilike(term)).
 
+    if sort=="hot":
+        posts=posts.order_by(text("submissions.rank_hot desc"))
+    elif sort=="new":
+        posts=posts.order_by(Submission.created_utc.desc())
+    elif sort=="fiery":
+        posts=posts.order_by(text("submissions.rank_fiery desc"))
+    elif sort=="top":
+        posts=posts.order_by(text("submissions.score desc"))
+        
+    total=posts.count()
+    posts=[x.id for x in posts.offset(25*(page-1)).limit(26).all()]
+    
     next_exists=(len(results)==26)
     results=results[0:25]
 
-    return render_template("search.html", v=v, total=total, listing=results, next_exists=next_exists)
+    return render_template("search.html", v=v, total=total, listing=results, sort_method=sort_method, next_exists=next_exists)
